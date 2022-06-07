373b7c4e2329caa5d4d3aaa5d320a7d0b5e313d9
Mat Hornbeek <84995001+m4wh6k@users.noreply.github.com>
2022-06-06 19:24:00 -0700

    initial commit (#1)
    Co-authored-by: m4wh6k <m4wh6k@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ansible-tailscale-inventory
-Dynamic Ansible inventory for your Tailscale hosts
+Dynamic Ansible inventory for your Tailscale hosts. Allows you to reach your
+Tailscale hosts easily with Ansible.
+
+## Usage
+From one of your Tailscale nodes on your network, make
+`ansible-tailscale-inventory.py` available as an inventory to Ansible. This can
+be done as an argument to the `-i` option on Ansible commands, or by setting
+the `ANSIBLE_INVENTORY` environment variable's value as the path to the script.
+
+Note: At the time of writing, the inventory script has been tested with macOS
+and Linux, but not Windows.
+
+## Ansible Groups
+`ansible-tailscale-inventory.py` automatically provides a few groups.
+There are groups of hosts for each operating system (`macOS`, `linux`, etc).
+Online hosts are found in the `online` group, offline hosts in the `offline`
+group. The `self` group includes the local host.
+
+## Inventory Metadata
+The inventory automatically adds all available Tailscale IPs as a list in the
+fact `tailscale_ips`.
+
+## Contributing
+Contributions are welcomed. Please feel free to submit or fix issues.
+Please format python with `black`.
---
+++ ansible-tailscale-inventory.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import json
+import platform
+import subprocess
+
+if platform.system() == "Linux":
+    tailscale_cmd = "tailscale"
+elif platform.system() == "Darwin":
+    tailscale_cmd = "/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+
+tailscale_proc = subprocess.run(
+    [tailscale_cmd, "status", "--self", "--json"],
+    capture_output=True,
+)
+tailscale_output_json = json.loads(tailscale_proc.stdout)
+
+inventory = {
+    "_meta": {"hostvars": {}},
+    "all": {"hosts": []},
+    "online": {"hosts": []},
+    "offline": {"hosts": []},
+    "self": {"hosts": [tailscale_output_json["Self"]["HostName"]]},
+}
+
+all_hosts = list(
+    tailscale_output_json["Peer"].values(),
+)
+all_hosts.append(
+    tailscale_output_json["Self"],
+)
+
+for v in all_hosts:
+    inventory["all"]["hosts"].append(v["HostName"])
+
+    inventory["_meta"]["hostvars"][v["HostName"]] = {
+        "ansible_host": v["DNSName"],
+        "tailscale_ips": v["TailscaleIPs"],
+    }
+
+    if v["Online"]:
+        inventory["online"]["hosts"].append(v["HostName"])
+    else:
+        inventory["offline"]["hosts"].append(v["HostName"])
+
+    if v["OS"] not in inventory:
+        inventory[v["OS"]] = {"hosts": []}
+    inventory[v["OS"]]["hosts"].append(v["HostName"])
+
+print(json.dumps(inventory))
